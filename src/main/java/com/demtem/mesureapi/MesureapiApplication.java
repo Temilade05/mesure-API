80d7df1b041a0b9e17657b6989d3dcbb1ca6f98b
Temilade05 <temioladugba@gmail.com>
2021-01-12 09:44:56 +0100

    initial commit

---
+++ src/main/java/com/demtem/mesureapi/MesureapiApplication.java
@@ -0,0 +1,13 @@
+package com.demtem.mesureapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesureapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesureapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demtem/mesureapi/MesureapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demtem.mesureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesureapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
